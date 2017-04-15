573fb9760bec3069d00a235a23ec9ca4f5683b8e
ibrahimjaafar <ibrahimjaafar@live.com>
2017-04-15 13:38:28 -0400

    Added slope

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+var companyName, companySrc, companyTicker;
 window.onload = function () {
 	var url = document.location.href
 		, params = url.split('?')[1].split('&')
@@ -8,7 +9,7 @@ window.onload = function () {
 		data[tmp[0]] = tmp[1];
 	}
 	$.getJSON("mapping.json", function (res) {
-		var companyName, companySrc, companyTicker;
+		
 		for (i = 0; i < res.Records.length; i++) {
 			if (res.Records[i].Name.toLowerCase().includes(data.company.toLowerCase().split("+", 1))) {
 				companyTicker = res.Records[i].Ticker;
@@ -56,6 +57,17 @@ function getData(company) {
 			var data = {
 				labels: [resp.dataset.data[6][0], resp.dataset.data[5][0], resp.dataset.data[4][0], resp.dataset.data[3][0], resp.dataset.data[2][0], resp.dataset.data[1][0], resp.dataset.data[0][0]]
 				, datasets: [
+					
+				{
+						label: "Average"
+						, type:'line'
+						, backgroundColor: "rgba(56, 40, 160,0.1)"
+						, borderColor: "rgba(56, 40, 160,1)"
+						, borderWidth: 2
+						, hoverBackgroundColor: "rgba(56, 40, 160,0.7)"
+						, hoverBorderColor: "rgba(56, 40, 160,1)"
+						, data: [(resp.dataset.data[6][3] + resp.dataset.data[6][2])/2, (resp.dataset.data[5][3] + resp.dataset.data[5][2])/2,(resp.dataset.data[4][3] + resp.dataset.data[4][2])/2,(resp.dataset.data[3][3] + resp.dataset.data[3][2])/2,(resp.dataset.data[2][3] + resp.dataset.data[2][2])/2,(resp.dataset.data[1][3] + resp.dataset.data[1][2])/2,(resp.dataset.data[0][3]+ resp.dataset.data[0][2])/2]
+        , },
 					{
 						label: "High"
 						, backgroundColor: "rgba(74, 102, 68,1)"
@@ -74,6 +86,7 @@ function getData(company) {
 						, hoverBorderColor: "rgba(239, 82, 67,1)"
 						, data: [resp.dataset.data[6][3], resp.dataset.data[5][3], resp.dataset.data[4][3], resp.dataset.data[3][3], resp.dataset.data[2][3], resp.dataset.data[1][3], resp.dataset.data[0][3]]
 				, }
+			
 			]
 			};
 			var option = {
@@ -110,6 +123,7 @@ function getData(company) {
 					duration: 2000
 				}
 			};
+			
 			document.getElementById('lastDayTitle').innerHTML = "Stock Prices Over The Last Day";
 			var linegraph = Chart.Line(canvas2, {
 				data: data2
@@ -117,6 +131,13 @@ function getData(company) {
 			});
 			//
 			//7 day close
+			var slope = (resp.dataset.data[0][11] - resp.dataset.data[14][4])/13;
+			if (slope < 0){
+				slope = slope * -1;
+			}
+			else{
+				slope = slope + 1;
+			}
 			var canvas3 = document.getElementById('adjclose');
 			var data3 = {
 				labels: [resp.dataset.data[14][0], resp.dataset.data[13][0]
@@ -158,6 +179,21 @@ function getData(company) {
 			, resp.dataset.data[4][11], resp.dataset.data[3][11]
 			, resp.dataset.data[2][11], resp.dataset.data[1][11], resp.dataset.data[0][11]]
         , }
+		, {
+						label: "Adj Close price Slope"
+						, backgroundColor: "rgba(255,255,255,0)"
+						, borderColor: "rgba(43, 168, 66,1)"
+						, borderWidth: 2
+						, hoverBackgroundColor: "rgba(43, 168, 66,0.7)"
+						, hoverBorderColor: "rgba(43, 68, 66,1)"
+						, data: [resp.dataset.data[14][11], resp.dataset.data[14][11]+slope
+			, resp.dataset.data[14][11]+slope*2, resp.dataset.data[14][11]+slope*3
+			, resp.dataset.data[14][11]+slope*4, resp.dataset.data[14][11]+slope*5
+			, resp.dataset.data[14][11]+slope*6, resp.dataset.data[14][11]+slope*7
+			, resp.dataset.data[14][11]+slope*8, resp.dataset.data[14][11]+slope*9
+			, resp.dataset.data[14][11]+slope*10, resp.dataset.data[14][11]+slope*11
+			, resp.dataset.data[14][11]+slope*12, resp.dataset.data[14][11]+slope*13, resp.dataset.data[14][11]+slope*14]
+        , }
 
 			]
 			};
@@ -166,7 +202,9 @@ function getData(company) {
 				, animation: {
 					duration: 2000
 				}
+				
 			};
+			
 			var linegraph2 = Chart.Line(canvas3, {
 				data: data3
 				, options: option
@@ -240,13 +278,17 @@ function getNews(company) {
 			, // Request body
 			data: "{body}"
 		, }).done(function (data) {
-			for (i = 0; i < 8; i++) {
+			for (i = 0; i < 6; i++) {
 				var url = "\"" + data.value[i].url + "\"";
 				var name = data.value[i].name;
 				var date = data.value[i].datePublished;
 				if (data.value[i].image != null) {
 					var imgUrl = data.value[i].image.thumbnail.contentUrl;
 					document.getElementsByTagName("section")[i].innerHTML += "<img src=" + imgUrl + " class='news' alt='image'><br>"
+				}
+				else{
+					document.getElementsByTagName("section")[i].innerHTML += "<img src=" + companySrc + " class='news' alt='image'><br>"
+
 				}
 				var description = data.value[i].description;
 				document.getElementsByTagName("section")[i].innerHTML += "<a  href=" + url + "><p class='ind' id='title'>" + name + "</p></a>"
